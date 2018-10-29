6ab7b6b5ebb4fddac87d06c00d0beb58f94a39ff
michaelstsnow <michael.st.snow@gmail.com>
2018-10-29 08:29:24 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,9 @@
 def square_array(array)
   # your code here
   squared_array=[];
+  location = 0;
   array.each do |x|
-    squared_array << x^2;
+    squared_array[location]=x^2;
+    location += 1;
   end
 end
