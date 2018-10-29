79224ba9de3430f8fcc4dfffd68bb2d3e8316a5a
michaelstsnow <michael.st.snow@gmail.com>
2018-10-29 08:30:37 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,10 @@
 def square_array(array)
   # your code here
+  squared_array=[];
+  location = 0;
+  array.each do |x|
+    squared_array[location]=x*x;
+    location += 1;
+  end
+  return squared_array
 end
